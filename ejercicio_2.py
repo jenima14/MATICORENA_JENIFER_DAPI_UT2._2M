fcc462ca32ce3d4201022b49e36541747c644deb
jenima14 <jmarticarr2@educacion.navarra.es>
2023-12-22 10:29:16 +0100

    ejercicio_02

---
+++ ejercicio_2.py
@@ -0,0 +1,6 @@
+datos = {"tiempo":"km", "corredor":""}
+while true:
+    diccionario= int(input("Ingresa la hora: "))
+    if diccionario ==
+        lista = int(input("Ingresa los kilómetros"))
+        datos = bool(int(input))
